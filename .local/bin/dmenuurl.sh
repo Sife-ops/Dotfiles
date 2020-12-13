82ebe00210f33dfd5abc5a425be33eb3f141bebc
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2020-12-13 01:24:24 -0800

    removed dunst notifications

--- .local/bin/dmenuurl.sh
+++ .local/bin/dmenuurl.sh
@@ -4,62 +4,46 @@
 if which checkdeps.sh >/dev/null 2>&1; then
     checkdeps.sh dmenu || exit 1; fi
 
+alias dmenucmd='dmenu -b -i -l 20'
+which tsp 1>/dev/null 2>&1 && alias bgcmd='tsp' || alias bgcmd='setsid -f'
+
 msg_help() { echo \
 "Usage:
-    urlmenu.sh [URL]"
+    dmenuurl.sh [URL]"
 }
 
-alias st_cmd='st -c task -e sh -c'
-
-[ -z $1 ] \
-    && url="$(xclip -selection clipboard -o)" \
-    || url="$1"
+[ -z $1 ] && url="$(xclip -selection clipboard -o)" || url="$1"
 
 base="$(basename "$url")"
-chosen=$(printf " -> ${base} <-\\nadd feed\\nbrowser\\ncopy to clipboard\\ncurl\\nmpv\\nsxiv\\nyoutube-dl" |
-    dmenu -b -i -l 20 -p "action")
+
+chosen=$(echo \
+"File: $base
+add feed
+browser
+copy to clipboard
+curl
+mpv
+sxiv
+youtube-dl" | dmenucmd -p "action")
 
 case $chosen in
-    "add feed")
-        exit ;;
-
-	browser)
-        setsid -f "$BROWSER" "$url" >/dev/null 2>&1 ;;
-
-    "copy to clipboard")
-        echo "$url" | xclip -selection clipboard ;;
-
-    curl)
-        notify-send "⏳downloading $base"
-        st_cmd "curl -LO --output-dir ~/Downloads $url && notify-send \"👍$base done\"; $SHELL" ;;
-
-	mpv)
-        setsid -f mpv -quiet "$url" >/dev/null 2>&1 ;;
-
-    sxiv)
-        exit ;;
-
-    youtube-dl)
-        chosen=$(printf "automatic\\nbest quality\\naudio only\\naudio playlist" |
-            dmenu -b -i -l 20 -p "ytdl")
+    "add feed") echo $url >> ${HOME}/newsboat/urls ;;
+	browser) setsid -f "$BROWSER" "$url" >/dev/null 2>&1 ;;
+    "copy to clipboard") echo "$url" | xclip -selection clipboard ;;
+    curl) bgcmd curl -LO --output-dir ~/Downloads $url ;;
+	mpv) setsid -f mpv -quiet "$url" >/dev/null 2>&1 ;;
+    sxiv) tmp=$(mktemp /tmp/sxivXXX) && curl -L $url -o $tmp && setsid -f sxiv $tmp ;;
+    youtube-dl) chosen=$(echo \
+"automatic
+best quality
+audio only
+audio playlist" | dmenucmd -p "ytdl")
 
         case $chosen in
-            automatic)
-                notify-send "⏳downloading $base..."
-                st_cmd "youtube-dl -o ~/Downloads/%\(title\)s.%\(ext\)s $url && notify-send \"👍$base done\"; $SHELL" ;;
-
-            "best quality")
-                notify-send "⏳downloading $base..."
-                st_cmd "youtube-dl -f bestvideo+bestaudio -o ~/Downloads/%\(title\)s.%\(ext\)s $url && notify-send \"👍$base done\"; $SHELL" ;;
-
-            "audio only")
-                notify-send "⏳downloading $base..."
-                st_cmd "youtube-dl -x --audio-format mp3 -o ~/Downloads/%\(title\)s.%\(ext\)s "$url" && notify-send \"👍$base done\"; $SHELL" ;;
-
-            "audio playlist")
-                notify-send "⏳downloading $base..."
-                st_cmd "youtube-dl -i --extract-audio --audio-format mp3 -o ~/Downloads/%\(title\)s.%\(ext\)s "$url" && notify-send \"👍$base done\"; $SHELL" ;;
-
+            automatic) bgcmd youtube-dl -o ~/Downloads/%\(title\)s.%\(ext\)s $url ;;
+            "best quality") bgcmd youtube-dl -f bestvideo+bestaudio -o ~/Downloads/%\(title\)s.%\(ext\)s $url ;;
+            "audio only") bgcmd youtube-dl -x --audio-format mp3 -o ~/Downloads/%\(title\)s.%\(ext\)s "$url" ;;
+            "audio playlist") bgcmd youtube-dl -i --extract-audio --audio-format mp3 -o ~/Downloads/%\(title\)s.%\(ext\)s "$url" ;;
             *) exit ;;
         esac ;;
     *) exit ;;
