6e579e2688a3334d5e0aaa7ae699eb50bc7fd6a1
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-01-13 01:33:28 -0800

    fixed variable names

--- .local/bin/dmenuclipboard.sh
+++ .local/bin/dmenuclipboard.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # manage clipboards
 
+if which checkdeps.sh >/dev/null 2>&1; then
+    checkdeps.sh xclip dmenu || exit 1; fi
+
 alias dmenucmd='dmenu -b -i -l 20'
 
 chosen_a=$(echo \
@@ -16,13 +19,13 @@ clipboard
 user" | dmenucmd -p "target")
 
 case $chosen_a in
-    user) asdf="cat ${CLIPBOARD:-${XDG_DATA_HOME}/clipboard} " ;;
-    *) asdf="xclip -o -selection $chosen_a " ;;
+    user) cmd_a="cat ${CLIPBOARD:-${XDG_DATA_HOME}/clipboard} " ;;
+    *) cmd_a="xclip -o -selection $chosen_a " ;;
 esac
 
 case $chosen_b in
-    user) fdsa="> ${CLIPBOARD:-${XDG_DATA_HOME}/clipboard}" ;;
-    *) fdsa="| xclip -i -selection $chosen_b" ;;
+    user) cmd_b="> ${CLIPBOARD:-${XDG_DATA_HOME}/clipboard}" ;;
+    *) cmd_b="| xclip -i -selection $chosen_b" ;;
 esac
 
-eval "$asdf $fdsa"
+eval "$cmd_a $cmd_b"
