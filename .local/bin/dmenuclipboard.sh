032bb151f585797aa10b7bc819c0d177932ec836
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-01-18 23:53:27 -0800

    added more features

--- .local/bin/dmenuclipboard.sh
+++ .local/bin/dmenuclipboard.sh
@@ -6,30 +6,63 @@ if which checkdeps.sh >/dev/null 2>&1; then
 
 alias dmenucmd='dmenu -b -i -l 20'
 
-chosen_a=$(echo \
-"primary: $(xclip -o -selection primary | head -n1)
-secondary: $(xclip -o -selection secondary | head -n1)
-clipboard: $(xclip -o -selection clipboard | head -n1)
-user: $(head -n1 ${CLIPBOARD:-${HOME}/.local/share/clipboard})" |
+user_clipboard="${CLIPBOARD:-${HOME}/.local/share/clipboard}"
+user_clipboard_dir="${CLIPBOARD_DIR:-${HOME}/.local/share/clipboard.d}"
+
+function menu_a () {
+    echo "~# Xorg #~"
+    echo "clipboard: $(xclip -o -selection clipboard | head -n1)"
+    echo "primary: $(xclip -o -selection primary | head -n1)"
+    echo "secondary: $(xclip -o -selection secondary | head -n1)"
+
+    if [ -f "$user_clipboard" ]; then
+        echo "~# User #~"
+        echo "user: $(head -n1 $user_clipboard)"
+    fi
+
+    if [ -d "$user_clipboard_dir" ]; then
+        [ -f "$user_clipboard" ] || echo "~# User #~"
+        for file in $(find $user_clipboard_dir -type f -exec basename {} \;); do
+            echo "user/${file}: $(head -n1 ${user_clipboard_dir}/${file})"
+        done | sort
+    fi
+}
+
+function menu_b () {
+    # menu_b $chosen_a
+    if [ -d "$user_clipboard_dir" ]; then
+        echo "$1" | grep '^user\/.*' && \
+            echo "delete user clipboard: $1"
+        echo "new user clipboard: ..."
+    fi
+}
+
+chosen_a=$(menu_a |
     dmenucmd -p "source" |
     cut -d':' -f1)
-[ -z $chosen_a ] && exit 1
+[ -z "$chosen_a" ] && exit 1
 
-chosen_b=$(echo \
-"primary
-secondary
-clipboard
-user" | dmenucmd -p "target")
-[ -z $chosen_b ] && exit 1
+chosen_b=$(cat <(menu_a) <(menu_b "$chosen_a") |
+    dmenucmd -p "target" |
+    cut -d':' -f1)
+[ -z "$chosen_b" ] && exit 1
 
 case $chosen_a in
-    user) cmd_a="cat ${CLIPBOARD:-${XDG_DATA_HOME}/clipboard} " ;;
-    *) cmd_a="xclip -o -selection $chosen_a " ;;
+    primary|secondary|clipboard) cmd_a="xclip -o -selection $chosen_a" ;;
+    user/*) cmd_a="cat ${user_clipboard_dir}/$(basename $chosen_a)" ;;
+    user) cmd_a="cat $user_clipboard" ;;
 esac
 
 case $chosen_b in
-    user) cmd_b="> ${CLIPBOARD:-${XDG_DATA_HOME}/clipboard}" ;;
-    *) cmd_b="| xclip -i -selection $chosen_b" ;;
+    primary|secondary|clipboard) cmd_b="| xclip -i -selection $chosen_b" ;;
+    user/*) cmd_b="> ${user_clipboard_dir}/$(basename $chosen_b)" ;;
+    user) cmd_b="> $user_clipboard" ;;
+    "new user clipboard")
+        cmd_b="> ${user_clipboard_dir}/$(echo "" | dmenu -b -p "name")" ;;
+    "delete user clipboard")
+        rm "${user_clipboard_dir}/$(basename $chosen_a)"
+        exit 0 ;;
 esac
 
 eval "$cmd_a $cmd_b"
+# echo "$cmd_a $cmd_b" # debug
