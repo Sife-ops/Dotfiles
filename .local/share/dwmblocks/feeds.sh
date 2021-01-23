c057ecc7b3be5ed47d70f93106a62641f746885d
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-01-23 11:04:00 -0800

    added feeds block

---
+++ .local/share/dwmblocks/feeds.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# print unread feeds
+
+unread=$(tac $NOTIFICATIONS 2>/dev/null |
+    grep 'newsboat' 2>/dev/null |
+    head -n1 |
+    jq -r '.summary' |
+    sed -r 's/.*\(([0-9]+)( unread articles).*/\1/')    # POSIX rewrite
+icon="📰"
+
+if test "$unread" -eq "$unread" >/dev/null 2>&1; then   # evil hack
+    if [ "$unread" -lt "1" ]; then
+        printf " "
+    else
+        printf "%s%s" "$icon" "$unread"
+    fi
+else
+    printf " "
+    exit 0
+fi
+
