57af3bb7e41604b715aa2a76d5690d6d24e7ca7b
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-01-23 21:10:35 -0800

    added login and sync options

--- .local/bin/dmenubw.sh
+++ .local/bin/dmenubw.sh
@@ -1,47 +1,96 @@
 #!/bin/bash
 # dmenu interface for bitwarden-cli
+# todo:
+# create mode
+# edit mode
 
+#^ setup
 shopt -s expand_aliases
 alias bw_cmd="bw --nointeraction"
-alias dmenu_cmd="${DMENUCMD:-dmenu -b -i -l 20}"
-alias prompt_cmd="zenity --password"
+alias dmenu_cmd="${DMENU_CMD:-dmenu -b -i -l 20}"
+alias prompt_cmd="printf '' | dmenu_cmd -p 'Password:'"
+# alias prompt_cmd="zenity --password"
 # alias prompt_cmd='yad --entry --entry-label="Password: " --hide-text'
-
-password_cache="${BW_GPG_FILE:-$HOME/.cache/bw.gpg}"
 
 if which checkdeps.sh >/dev/null 2>&1; then
     checkdeps.sh dmenu bw gpg jq xclip ${prompt_cmd%% *} || exit 1; fi
 
-if [ ! -f "$password_cache" ]; then
-    sessionkey=$(bw_cmd unlock "$(prompt_cmd)" |
-        grep 'export' |
-        sed 's/^.*BW_SESSION="\(.*\)"/\1/')
-    if bw_cmd list items --session "$sessionkey" >/dev/null; then
-        echo "$sessionkey" |
-            gpg --quiet --recipient "$BW_GPG_NAME" \
-                --encrypt --output "$password_cache"
-    else
-        exit 1
-    fi
-else
-    sessionkey=$(gpg --quiet --decrypt "$password_cache")
+session_cache="${BW_SESSION_CACHE:-$HOME/.cache/bw-session.gpg}"
+if [ -z "$BW_GPG_ID" ]; then
+    printf "" | dmenu_cmd -p "Error: BW_GPG_ID must be set."
+    exit 1
 fi
+#$
 
-# jq -r '.[] | "\(.name) | username: \(.login.username) | id: \(.id)"' |
-id=$(bw_cmd list items --session "$sessionkey" |
-    jq -r '.[] | "\(.name) | id: \(.id)"' |
-    dmenu_cmd -p "item" |
+function login(){ #^
+    if [ ! -f "$session_cache" ]; then
+        session_key=$(bw_cmd unlock "$(prompt_cmd)" |
+            grep 'export' |
+            sed 's/^.*BW_SESSION="\(.*\)"/\1/')
+        if bw_cmd list items --session "$session_key" >/dev/null; then
+            echo "$session_key" |
+                gpg --quiet --recipient "$BW_GPG_ID" \
+                    --encrypt --output "$session_cache"
+        else
+            exit 1
+        fi
+    else
+        session_key=$(gpg --quiet --decrypt "$session_cache")
+    fi
+} #$
+
+function print_items(){ #^
+    # jq -r '.[] | "\(.name) | username: \(.login.username) | id: \(.id)"' |
+    bw_cmd list items --session "$session_key" |
+        jq -r '.[] | "\(.name) | id: \(.id)"'
+} #$
+
+function logout(){ #^
+    rm -f "$session_cache"
+    exit 0
+} #$
+
+function sync(){ #^
+    bw_cmd sync --session "$session_key"
+    exit 0
+} #$
+
+function get(){ #^
+    id=$(printf "$1" |
     cut -d '|' -f 2 |
     cut -d ':' -f 2 |
     tr -d '[:space:]')
 
-# chosen=$(printf "username\npassword" | dmenu_cmd -p "field")
-output=$(bw_cmd get item --session "$sessionkey" "$id" |
-    jq -r ".login.username, .login.password")
+    # chosen=$(printf "username\npassword" | dmenu_cmd -p "field")
+    output=$(bw_cmd get item --session "$session_key" "$id" |
+        jq -r ".login.username, .login.password")
+    printf "$output" |
+        sed -n 1p |
+        xclip -i -selection clipboard
+    printf "$output" |
+        sed -n 2p |
+        xclip -i -selection primary
 
-echo "$output" |
-    sed -n 1p |
-    xclip -i -selection clipboard
-echo "$output" |
-    sed -n 2p |
-    xclip -i -selection primary
+    exit 0
+} #$
+
+login
+
+#^ main menu
+chosen=$(cat \
+        <(print_items) \
+        <(printf "Create ...\n") \
+        <(printf "Logout ...\n") \
+        <(printf "Sync ...\n") |
+    dmenu_cmd -p "item")
+
+case "$chosen" in
+    "Create ...") exit 0 ;;
+    "Logout ...") logout ;;
+    "Sync ...") sync ;;
+    "") exit 1 ;;
+    *) get "$chosen" ;;
+esac
+#$
+
+# vim: fdm=marker fmr=#^,#$
