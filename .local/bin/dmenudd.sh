4fd54cd4b03d41484688ed2a3d2e8d52bf86fdea
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-01-25 01:35:06 -0800

    added dmenudd.sh script

---
+++ .local/bin/dmenudd.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# toggle dropdown window
+
+#^ setup
+if which checkdeps.sh 1>/dev/null 2>&1; then
+    checkdeps.sh xdotool xwininfo dmenu || exit 1; fi
+
+alias dmenu_cmd="${DMENU_CMD:-dmenu -b -i -l 20}"
+#$
+
+print_ids(){ #^
+    for win in $(xdotool search --class "dd"); do
+        printf '%s: %s\n' "$win" "$(xdotool getwindowname "$win")"
+    done
+} #$
+
+togmap(){ #^
+    mapstate=$(xwininfo -id "$1" | awk '/Map State/{print $3}')
+
+    case "$mapstate" in
+        IsViewable)
+            xdotool windowunmap "$1" ;;
+        IsUnMapped)
+            xdotool windowmap "$1" ;;
+    esac
+} #$
+
+#^ main
+chosen=$(print_ids | dmenu_cmd -p "dd" | cut -d ':' -f 1)
+case "$chosen" in
+    "")
+        exit 1
+        ;;
+    *)
+        togmap "$chosen"
+        ;;
+esac
+#$
+
+# vim: fdm=marker fmr=#^,#$
