2dd0a3ad6ed3f3eecb4a3e0acdde71460a83f448
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-01-31 07:01:18 -0800

    added tmux startup script

---
+++ .config/tmux/default.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+tmux start-server
+
+session="default"
+win0="default"
+
+tmux new-session -d -s "$session" -n "$win0"
+
+tmux split-window -t "${session}:${win0}.0" -h
+tmux split-window -t "${session}:${win0}.0" -v
+tmux split-window -t "${session}:${win0}.2" -v
+
+# -------------
+# |  0  |  2  |
+# -------------
+# |  1  |  3  |
+# -------------
+
+tmux send-keys -t "${session}:${win0}.0" "neomutt" C-m
+tmux send-keys -t "${session}:${win0}.1" "newsboat" C-m
+tmux send-keys -t "${session}:${win0}.2" "irssi" C-m
