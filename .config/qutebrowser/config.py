315111019fedc41d7b53a01da1be15dbdff10c49
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-02-01 21:19:30 -0800

    enabled dracula theme

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,7 +1,26 @@
 config.load_autoconfig()
 
-c.content.user_stylesheets = ['~/.config/qutebrowser/css/solarized-dark-all-sites.css']
+import dracula.draw
+
+# Load existing settings made via :set
+config.load_autoconfig()
+
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
+
+c.fonts.default_size = '16pt'
+c.fonts.web.size.default = 20
+c.fonts.web.size.default_fixed = 16
+
+c.content.user_stylesheets = ['~/.config/qutebrowser/css/gruvbox-all-sites.css']
 config.bind('<Ctrl-R>', 'config-cycle content.user_stylesheets "~/.config/qutebrowser/css/gruvbox-all-sites.css" "~/.config/qutebrowser/css/solarized-dark-all-sites.css" ""')
+
 config.bind('J', 'tab-prev')
 config.bind('K', 'tab-next')
+config.bind('d', 'scroll-page 0 0.5')
+config.bind('u', 'scroll-page 0 -0.5')
 config.bind('x', 'tab-close')
