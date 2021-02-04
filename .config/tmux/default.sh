58f69c281010b39f29055e911e028a5c6e13c5b9
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-02-03 16:30:35 -0800

    changed colors

--- .config/tmux/default.sh
+++ .config/tmux/default.sh
@@ -2,21 +2,25 @@
 
 tmux -f ${XDG_CONFIG_HOME:-$HOME/.config}/tmux/tmux.conf start-server
 
-session="default"
-win0="default"
+name="default"
 
-tmux new-session -d -s "$session" -n "$win0"
+tmux new-session -d -s "$name" -n "$name"
 
-tmux split-window -t "${session}:${win0}.0" -h
-tmux split-window -t "${session}:${win0}.0" -v
-tmux split-window -t "${session}:${win0}.2" -v
+tmux split-window -t "${name}:0.0" -v
+tmux split-window -t "${name}:0.0" -v
+tmux select-layout -E -t "${name}:0"
+tmux split-window -t "${name}:0.0" -h
+tmux split-window -t "${name}:0.2" -h
 
-# -------------
-# |  0  |  2  |
-# -------------
-# |  1  |  3  |
-# -------------
+# -------------------------
+# |     0     |     2     |
+# -------------------------
+# |     1     |     3     |
+# -------------------------
+# |           4           |
+# -------------------------
 
-tmux send-keys -t "${session}:${win0}.0" "neomutt" C-m
-tmux send-keys -t "${session}:${win0}.1" "newsboat" C-m
-tmux send-keys -t "${session}:${win0}.2" "irssi" C-m
+tmux send-keys -t "${name}:0.0" "neomutt" C-m
+tmux send-keys -t "${name}:0.1" "irssi" C-m
+tmux send-keys -t "${name}:0.2" "newsboat" C-m
+tmux send-keys -t "${name}:0.4" "pulsemixer" C-m
