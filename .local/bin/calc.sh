8eb59542f5d7615d97d497cb8ec96b741a6c5689
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-02-10 20:44:52 -0800

    moved math functions to separate file

--- .local/bin/calc.sh
+++ .local/bin/calc.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # dmenu unit calculator
+# sinitic numbers
+# currency?
 
+. math.sh
 . menu.sh
-
-if which checkdeps.sh 1>/dev/null 2>&1; then
-    checkdeps.sh bc || exit 1; fi
 
 export BC_LINE_LENGTH=0
 
@@ -24,73 +24,22 @@ if echo "$input" | grep '\.' 1>/dev/null 2>&1; then
     dots="${#dots}"
 fi
 
-hexadecimal=1
-decimal=1
-binary=1
-octal=1
-printf '%s' "$input" | grep '[A-F]' >/dev/null && unset decimal && unset binary && unset octal
-printf '%s' "$input" | grep '[8-9]' >/dev/null && unset binary && unset octal
-printf '%s' "$input" | grep '[2-7]' >/dev/null && unset binary
-
-#^
-bc_(){
-    echo "$1" | bc
-}
-
-h2b(){
-    bc_ "ibase=16; obase=2; $1"
-}
-
-h2d(){
-    bc_ "ibase=16; $1"
-}
-
-h2o(){
-    bc_ "ibase=16; obase=8; $1"
-}
-
-d2b(){
-    bc_ "obase=2; $1"
-}
-
-d2h(){
-    bc_ "obase=16; $1"
-}
-
-d2o(){
-    bc_ "obase=8; $1"
-}
-
-o2b(){
-    bc_ "ibase=8; obase=2; $1"
-}
-
-o2d(){
-    bc_ "ibase=8; $1"
-}
-
-o2h(){
-    bc_ "obase=16; ibase=8; $1"
-}
-
-b2d(){
-    bc_ "ibase=2; $1"
-}
-
-b2h(){
-    bc_ "ibase=2; obase=10000; $1"
-}
-
-b2o(){
-    bc_ "ibase=2; obase=1000; $1"
-}
-
 dotted(){
     for i in $(seq $((dots + 1))); do
         $1 "$(echo "$input" | cut -d '.' -f "$i")"
     done | paste -d '.' $(seq -s ' ' $((dots + 1)) | tr '[:alnum:]' '-')
 }
-#$
+
+decimal=1
+binary=1
+octal=1
+if echo "$input" | grep '[A-F]' >/dev/null; then
+    input_decimal="$(h2d "$input")"
+    unset decimal; unset binary; unset octal; fi
+if echo "$input" | grep '[8-9]' >/dev/null; then
+    unset binary; unset octal
+if echo "$input" | grep '[2-7]' >/dev/null; then
+    unset binary
 
 #^
 if [ -n "$dots" ]; then
@@ -128,16 +77,21 @@ $(printf 'bin -> oct: %s' "$(b2o "$input")")"
 fi
 #$
 
-main_list="$hex
+main_list="~= base conversion =~
+$hex
 $([ -n "$decimal" ] && echo "$dec")
 $([ -n "$octal" ] && echo "$oct")
-$([ -n "$binary" ] && echo "$bin")"
+$([ -n "$binary" ] && echo "$bin")
+~= unit conversion =~"
 main_list="$(echo "$main_list" | sed '/^$/d')"
 
-choose "$main_list" "input"
+choose "$main_list" "$input"
 case "$chosen" in
     "") exit 1 ;;
-    *) clipboard yank "$(echo "$chosen" | cut -d ':' -f 2 | tr -d "[:space:]")" ;;
+    "~="*) exit 1 ;;
+    *) clipboard yank "$(echo "$chosen" |
+        cut -d ':' -f 2 |
+        tr -d "[:space:]")" ;;
 esac
 
 # vim: fdm=marker fmr=#^,#$
---
+++ .local/bin/math.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+# math functions
+
+if which checkdeps.sh 1>/dev/null 2>&1; then
+    checkdeps.sh bc || exit 1; fi
+
+bc_(){
+    echo "$1" | bc
+}
+
+h2b(){
+    bc_ "ibase=16; obase=2; $1"
+}
+
+h2d(){
+    bc_ "ibase=16; $1"
+}
+
+h2o(){
+    bc_ "ibase=16; obase=8; $1"
+}
+
+d2b(){
+    bc_ "obase=2; $1"
+}
+
+d2h(){
+    bc_ "obase=16; $1"
+}
+
+d2o(){
+    bc_ "obase=8; $1"
+}
+
+o2b(){
+    bc_ "ibase=8; obase=2; $1"
+}
+
+o2d(){
+    bc_ "ibase=8; $1"
+}
+
+o2h(){
+    bc_ "obase=16; ibase=8; $1"
+}
+
+b2d(){
+    bc_ "ibase=2; $1"
+}
+
+b2h(){
+    bc_ "ibase=2; obase=10000; $1"
+}
+
+b2o(){
+    bc_ "ibase=2; obase=1000; $1"
+}
