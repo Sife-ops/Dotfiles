cb0c1eb0c46c33083ca7c5f288e888ef38346d66
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-02-10 20:56:56 -0800

    added edit function

--- .local/bin/bookmarks.sh
+++ .local/bin/bookmarks.sh
@@ -5,11 +5,13 @@
 
 bookmarks="${BOOKMARKS:-${XDG_DATA_HOME:-${HOME}/.local/share}/bookmarks}"
 
-main_list="$(grep -v '^#' "$bookmarks" | sed '/^$/d' | sort)"
+main_list="$(grep -v '^#' "$bookmarks" | sed '/^$/d' | sort)
+Edit ..."
 
 choose "$main_list" "bookmarks"
 url=$(echo "$chosen" | cut -d '|' -f 2 | tr -d "[:space:]")
 case "$chosen" in
+    "Edit ...") buffer "$bookmarks" ;;
     "") exit 1 ;;
     *) url.sh $url ;;
 esac
