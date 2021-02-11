ba5163c1ba59a26b06ba8040b66f5ea6e94649b5
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-02-10 23:53:21 -0800

    changed to auto installer

--- .local/share/sarbs/install.sh
+++ .local/share/sarbs/install.sh
@@ -1,95 +1,40 @@
 #!/bin/sh
 
-msg_help() { echo \
-"Usage:
-    install.sh [OPTIONS]
+# after partitioning
+pacstrap /mnt \
+    base \
+    base-devel \
+    linux \
+    linux-firmware \
+    vim \
+    git \
+    efibootmgr \
+    grub \
+    networkmanager \
+    openssh
 
-Options:
-    -a  install all
-    -B  install barrierc on XDM
-    -b  install bspwm
-    -c  install crontab
-    -H  install the hosts file
-    -p  install pacman options
-    -h  print this message"
-}
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
 
-while getopts "aBbcHph" o; do case "${o}" in
-	a) tasks=("installBarrierc" "installBspwm" "installCrontab" "installHosts" "installPacman") && break ;; # array of all tasks
-	B) tasks+=("installBarrierc") ;;
-	b) tasks+=("installBspwm") ;;
-	c) tasks+=("installCrontab") ;;
-	H) tasks+=("installHosts") ;;
-	p) tasks+=("installPacman") ;;
-    h) msg_help ;;
-	*) printf "Invalid option: -%s\\n" "$o" && msg_help && exit 1 ;;
-esac done
-[ "$#" -lt "1" ] && msg_help && exit 1
+# after chroot
+ln -sf /usr/share/zoneinfo/US/Pacific /etc/localtime
+hwclock --systohc
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+echo 'ja_JP.UTF-8 UTF-8' >> /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+echo 'nothingburger' > /etc/hostname
+echo '127.0.0.1	localhost
+::1		localhost
+127.0.1.1	nothingburger.geofront	nothingburger' > /etc/hosts
+passwd
+systemctl enable NetworkManager
+systemctl enable sshd
+echo 'PermitRootLogin yes' > /etc/ssh/sshd_config
 
-function installXorg() {
-    echo imgay
-}
+### UEFI only
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=sarbs
+### BIOS only
+grub-install --target=i386-pc /dev/sda
 
-function installBspwm() {
-    installXorg
-}
-
-function installCrontab(){
-    if [ ! -e /var/spool/cron/$(id -un) ]
-    then
-        echo \
-"@reboot /usr/bin/pacman -Qqen > ~/.local/share/sarbs/profiles/$(uname -n)/.local/share/sarbs/paclist
-@reboot /usr/bin/pacman -Qqem > ~/.local/share/sarbs/profiles/$(uname -n)/.local/share/sarbs/aurlist" \
-        > /var/spool/cron/$(id -un)
-    fi
-
-    if [ ! -e /var/spool/cron/root ]
-    then
-        echo \
-"@hourly /usr/bin/pacman -Sy" \
-        > /var/spool/cron/root
-    fi
-}
-
-function installHosts() {
-    if [ "$(wc -l /etc/hosts | cut -d' ' -f1)" -lt "3" ]
-    then
-        echo \
-"# Static table lookup for hostnames.
-# See hosts(5) for details.
-127.0.0.1           localhost
-::1                 localhost
-127.0.1.1           $(uname -n).geofront    $(uname -n)
-
-# 192.168.69.1        russianbot.geofront     russianbot
-# 192.168.69.2        casper.geofront         casper
-# 192.168.69.3        nothingburger.geofront  nothingburger
-# 192.168.69.4        45savage.geofront       45savage" \
-        > /etc/hosts
-    fi
-}
-
-function installPacman() {
-    if ! grep '^Color$' /etc/pacman.conf >/dev/null 2>&1
-    then
-        sed -i '/^# Misc options$/a Color' /etc/pacman.conf
-    fi
-    if ! grep '^ILoveCandy$' /etc/pacman.conf >/dev/null 2>&1
-    then
-        sed -i '/^# Misc options$/a ILoveCandy' /etc/pacman.conf
-    fi
-}
-
-function installBarrierc() {
-    if [ "$(cat /etc/hostname)" != "casper" ] \
-        && [ -f /etc/X11/xdm/Xsetup_0 ] \
-        && ! grep '^barrierc casper&$' /etc/X11/xdm/Xsetup_0 >/dev/null 2>&1
-        then
-            echo 'barrierc casper &' >> /etc/X11/xdm/Xsetup_0
-    fi
-}
-
-for task in ${tasks[@]}
-do
-    eval $task
-done
+grub-mkconfig -o /boot/grub/grub.cfg
