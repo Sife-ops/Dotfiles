080f68d7045d6ed94f5dc65111907eb9e2fb5b1f
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-02-14 04:03:18 -0800

    added edit function

--- .local/bin/bookmarks.sh
+++ .local/bin/bookmarks.sh
@@ -3,17 +3,31 @@
 
 . menu.sh
 
-bookmarks="${BOOKMARKS:-${XDG_DATA_HOME:-${HOME}/.local/share}/bookmarks}"
+config="${XDG_CONFIG_HOME:-${HOME}/.config}/bookmarks/config"
+while IFS= read -r line; do
+    case "$line" in
+        '#'*) : ;;
+        *) eval "$line" ;;
+    esac
+done < "$config"
 
-main_list="$(grep -v '^#' "$bookmarks" | sed '/^$/d' | sort)
-Edit ..."
+[ ! -f "$BOOKMARKS" ] && curl "$BOOKMARKS_GIST" > "$BOOKMARKS"
+gist="$(echo "$BOOKMARKS_GIST" | cut -d '/' -f 5)"
+gist="https://gist.github.com/$gist"
+
+main_list="$(grep -v '^#' "$BOOKMARKS" | sed '/^$/d' | sort)
+Edit ...
+Sync ..."
 
 choose "$main_list" "bookmarks"
 url=$(echo "$chosen" | cut -d '|' -f 2 | tr -d "[:space:]")
 case "$chosen" in
-    "Edit ...") buffer "$bookmarks" ;;
+    "Edit ...")
+        gh auth login --with-token "$(gpg --decrypt "$BOOKMARKS_TOKEN")"
+        if [ -n "$nox" ]; then
+            gh gist edit "$gist"; else
+            st -e gh gist edit "$gist"; fi ;;
+    "Sync ...") curl "$BOOKMARKS_GIST" > "$BOOKMARKS" ;;
     "") exit 1 ;;
     *) url.sh $url ;;
 esac
-
-
