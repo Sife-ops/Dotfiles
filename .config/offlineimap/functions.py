88ab780d02a2b31b46da605913964131d33644f0
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-02-18 05:33:35 -0800

    fixed homedir variable

--- .config/offlineimap/functions.py
+++ .config/offlineimap/functions.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python2
 from subprocess import check_output
 
+homedir = '/home/wyatt/.local/share/gnupg/'
 secrets = '/home/wyatt/.local/share/secrets/'
 def get_pass(pwfile):
-    return check_output("/usr/bin/gpg --homedir "+secrets+" -dq "+secrets+pwfile, shell=True).strip("\n")
+    return check_output("/usr/bin/gpg --homedir "+homedir+" -dq "+secrets+pwfile, shell=True).strip("\n")
