9a1253bed9a28f9ebf650614d39777e1daf4254d
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-02-26 08:38:05 -0800

    added if statement for argument

--- .local/bin/setwallpaper.sh
+++ .local/bin/setwallpaper.sh
@@ -2,5 +2,8 @@
 
 wallpaper="${WALLPAPER:-${XDG_DATA_HOME:-${HOME}/.local/share}/wallpaper}"
 
-ln -sf "$(readlink -f $1)" "$wallpaper"
+if [ -n "$1" ]; then
+    ln -sf "$(readlink -f $1)" "$wallpaper"
+fi
+
 xwallpaper --zoom "$wallpaper"
