e6684b67e22437fe60bd13cb36d86c8933569a06
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-03-07 13:21:26 -0800

    fixed xrandr command

--- .local/share/profiles/casper/home/wyatt/.config/i3/startup.sh
+++ .local/share/profiles/casper/home/wyatt/.config/i3/startup.sh
@@ -1,29 +1,7 @@
 #!/bin/sh
 
 sleep 1
-
-xrandr \
-    --dpi 108 \
-    --output DP-0 \
-        --off \
-    --output DP-1 \
-        --off \
-    --output DP-2 \
-        --off \
-    --output DP-3 \
-        --off \
-    --output HDMI-0 \
-        --mode 1360x768 \
-        --pos 1440x1792 \
-        --rotate normal \
-    --output DP-4 \
-        --left-of HDMI-0 \
-        --mode 2560x1440 \
-        --pos 0x0 \
-        --rotate left \
-    --output DP-5 \
-        --off
-
+xrandr --dpi 108 --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --mode 1360x768 --pos 0x1792 --rotate normal --output DP-4 --mode 2560x1440 --pos 1360x0 --rotate left --output DP-5 --off
 sleep 1
 
 setwallpaper.sh
