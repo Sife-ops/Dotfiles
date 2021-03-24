c33cf4f0d163095ea2f2d5138043edd3c9b62de9
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-03-23 23:21:33 -0700

    added getopts

--- .local/bin/ii_bridge.sh
+++ .local/bin/ii_bridge.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
-# usage: ii_bridge.sh FROM_OUT TO_IN [IGNORE]
 
-from="$1"
-to="$2"
-ignore="${3:+"<${3}>"}"
-
-tail -F -n 1 "$from" | while read a b c; do
-    case "$b" in
-        "$ignore") : ;;
-        "-!-") : ;;
-        *) echo "$c" > "$to" ;;
+while getopts "n:e" o; do
+    case "${o}" in
+        n) bridge_name="${OPTARG}" ;;
+        e) echo_nick="t" ;;
     esac
 done
+shift $((OPTIND - 1))
+
+bridge_name="<${bridge_name:-ii_bridge}>"
+from="$1"
+to="$2"
+
+tail -F -n 1 "$from" | while read a b c d; do
+    case "$b" in
+        "-!-") : ;;
+        *)
+            case "$c" in
+                "$bridge_name") : ;;
+                *) echo "$bridge_name ${echo_nick:+"${b}: "}$c $d" > "$to" ;;
+            esac ;;
+    esac
+done
+
--- .local/bin/ii_message.sh
+++ .local/bin/ii_message.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-message=$(mktemp /tmp/$(basename $PWD)_ii_XXX)
+message=$(mktemp /tmp/ii_message_$(basename $PWD)_XXX)
 
 while true; do
     truncate -s 0 "$message"
     ${EDITOR:-vim} "$message"
     chars=$(wc -m "$message" | cut -d ' ' -f 1)
     if [ "$chars" = "0" ]; then
+        rm "$message"
         exit
     else
         cat "$message" > "./in"
