0f152281ef708898f011fb03c22ac1f0719f9bbf
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-04-19 00:10:00 -0700

    removed personal configurations

--- .local/share/profiles/casper/home/wyatt/.local/bin/blocks/temperature.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# print CPU temperature (for Ryzen 7)
-
-temp=$(sensors 2>/dev/null \
-    | grep 'Tccd1' \
-    | awk '{ print $2 }')
-# printf "%s\\n" "$temp"
-icon="🌡"
-printf "%s%s\\n" "$icon" "$temp"
