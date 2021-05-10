9b71f495b43108ab3aab1f32f14c016d1588225c
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-05-10 02:35:54 -0700

    update

--- .local/share/scripts/crontab.sh
+++ .local/share/scripts/crontab.sh
@@ -2,8 +2,7 @@
 
 crontab="/var/spool/cron/$(id -un)"
 
-echo '@hourly export GNUPGHOME=/home/<+home+>/.local/share/gnupg; export SECRETS=/home/<+home+>/.local/share/secrets; /usr/bin/offlineimap -c ~/.config/offlineimap/offlineimaprc
-*/1 * * * * export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $USER)/bus; /home/<+home+>/.local/bin/pacnotifyupdate.sh' \
+echo '@hourly export GNUPGHOME=/home/<+home+>/.local/share/gnupg; export SECRETS=/home/<+home+>/.local/share/secrets; /usr/bin/offlineimap -c ~/.config/offlineimap/offlineimaprc' \
     > "$crontab"
 
 sed -i "s/<+home+>/$(id -un)/g" "$crontab"
