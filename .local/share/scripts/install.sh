a9264f34802b390ee0cdb5bc573e09424b741d43
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-05-23 21:53:07 -0700

    updates

--- .local/share/scripts/install.sh
+++ .local/share/scripts/install.sh
@@ -7,35 +7,50 @@ pacstrap /mnt \
     linux \
     linux-firmware \
     vim \
-    git \
-    efibootmgr \
-    grub \
-    networkmanager \
-    man \
-    tmux \
-    openssh
 
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 
 # after chroot
 ln -sf /usr/share/zoneinfo/US/Pacific /etc/localtime
+
 hwclock --systohc
+
 echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
 echo 'ja_JP.UTF-8 UTF-8' >> /etc/locale.gen
+
 locale-gen
+
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 echo 'coolname' > /etc/hostname
 echo '127.0.0.1	localhost
 ::1		localhost
 127.0.1.1	coolname.geofront	coolname' > /etc/hosts
+
 passwd
+
+pacman -S \
+    git \
+    efibootmgr \
+    grub \
+    networkmanager \
+    man \
+    tmux \
+    openssh \
+    xorg-server \
+    xorg-xinit \
+    xfce4 \
+    xfce4-goodies \
+    lightdm \
+    lightdm-gtk-greeter
+
 systemctl enable NetworkManager
 systemctl enable sshd
+
 echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 
 ### UEFI only
-grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=sarbs
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=arch
 ### BIOS only
 grub-install --target=i386-pc /dev/sda
 
