46e40670060d1aac7d16f263e041aff249e5dcf5
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-07-23 16:44:54 -0700

    show git files

--- nvim/.config/nvim/lua/config/nvim-tree.lua
+++ nvim/.config/nvim/lua/config/nvim-tree.lua
@@ -2,9 +2,13 @@ require('nvim-tree').setup({
   -- view = {
   --   side = 'right',
   -- },
+  open_on_setup = true,
   filters = {
     dotfiles = false,
   },
+  git = {
+    ignore = false,
+  }
 })
 require('keymap').nvim_tree()
 
