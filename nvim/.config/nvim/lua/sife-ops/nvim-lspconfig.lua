0e97dbb733837f3d52a3e7fa48ea808922d208b7
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-07-29 21:53:00 -0700

    move files

--- nvim/.config/nvim/lua/sife-ops/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/sife-ops/nvim-lspconfig.lua
@@ -3,29 +3,7 @@ local utility = require('sife-ops/utility')
 
 local opts = { noremap = true, silent = false }
 
--- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
--- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
--- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
--- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
-local on_attach = function(client, bufnr)
-  -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-  -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-  -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-  -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  -- vim.keymap.set('n', '<space>wl', function()
-  --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  -- end, bufopts)
-  -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  -- vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  -- vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
-
+local on_attach = function(_, bufnr)
   local set = vim.api.nvim_buf_set_keymap
 
   set(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
@@ -47,15 +25,9 @@ local on_attach = function(client, bufnr)
   set(bufnr, 'n', 'gs', '<cmd>vsplit<cr><cmd>lua vim.lsp.buf.definition()<cr>', opts)
   set(bufnr, 'n', 'gt', '<cmd>tab split<cr><cmd>lua vim.lsp.buf.definition()<cr>', opts)
   set(bufnr, 'v', 'ga', '<cmd>lua vim.lsp.buf.range_code_action()<cr>', opts)
-
 end
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
--- local lsp_flags = {
---   -- This is the default in Nvim 0.7+
---   debounce_text_changes = 150,
--- }
 
 lspconfig.tsserver.setup {
   cmd = {
@@ -102,15 +74,6 @@ lspconfig.sumneko_lua.setup {
     },
   },
 }
-
--- require('lspconfig')['rust_analyzer'].setup{
---     on_attach = on_attach,
---     flags = lsp_flags,
---     -- Server-specific settings...
---     settings = {
---       ["rust-analyzer"] = {}
---     }
--- }
 
 lspconfig.gopls.setup({
     cmd = {utility.lsp_dir .. '/gopls/gopls'},
--- nvim/.config/nvim/lua/sife-ops/packer.lua
+++ nvim/.config/nvim/lua/sife-ops/packer.lua
@@ -212,14 +212,53 @@ return packer.startup(function(use)
       { 'hrsh7th/cmp-nvim-lsp' },
       { 'hrsh7th/cmp-path' }
     },
-    config = function() require('sife-ops/config/nvim-cmp') end,
+    config = function()
+      local cmp = require('cmp')
+      cmp.setup({
+        experimental = { ghost_text = true },
+
+        snippet = {
+          expand = function(args)
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+          end,
+        },
+
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp', preselect = true },
+          { name = 'vsnip' }, -- For vsnip users.
+          -- { name = 'ultisnips', preselect = true },
+          {
+            name = 'buffer',
+            preselect = true,
+            max_item_count = 20,
+            option = { keyword_pattern = [[\k\k\k\+]] }
+          },
+          { name = 'path' },
+        }),
+
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+
+        mapping = cmp.mapping.preset.insert({
+          ['<C-Space>'] = cmp.mapping(cmp.mapping.complete({}), { 'i' }),
+          ['<CR>'] = cmp.mapping(cmp.mapping.confirm({ select = true }), { 'i' }),
+
+          -- sucks
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        }),
+      })
+    end
   }
   --$
 
   --^ nvim-lspconfig
   use {
     'neovim/nvim-lspconfig',
-    config = function() require('sife-ops/config/nvim-lspconfig') end,
+    config = function() require('sife-ops/nvim-lspconfig') end,
   }
   --$
 
