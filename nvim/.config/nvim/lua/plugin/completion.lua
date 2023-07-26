0fa2a7ba6deb89d14b2625722dd5b4a119df0bd2
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2023-07-26 12:22:24 -0700

    trigger completion

--- nvim/.config/nvim/lua/plugin/completion.lua
+++ nvim/.config/nvim/lua/plugin/completion.lua
@@ -67,6 +67,7 @@ return {
                 ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
                 ["<C-d>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
                 ["<C-e>"] = cmp.mapping.abort(),
                 ["<CR>"] = cmp.mapping.confirm({
                     behavior = cmp.ConfirmBehavior.Insert,
