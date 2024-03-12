fa1f514f41df1c791fdd3aa82137c1a394502790
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2024-03-12 00:24:12 -0700

    wip

--- ansible/pkl.sh
+++ ansible/pkl.sh
@@ -2,7 +2,7 @@
 find . -name \*.pkl -type f -print0 |
     while IFS= read -d '' file; do
         b=$(basename $file)
-        o="$(dirname $file)/${b%.pkl}.yml"
+        o=$(dirname $file)/${b%.pkl}.yml
         pkl eval -f yaml $file |
             sed '1 s@^.*$@#!/usr/bin/env ansible-playbook\n---@' > $o
         chmod +x $o
