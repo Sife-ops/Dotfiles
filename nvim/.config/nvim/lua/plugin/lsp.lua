cdb763706c3b24dd48e04a398f3ddc5862b4d24f
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2024-03-13 23:35:02 -0700

    wip

--- nvim/.config/nvim/lua/plugin/lsp.lua
+++ nvim/.config/nvim/lua/plugin/lsp.lua
@@ -9,22 +9,22 @@ return {
         { "hrsh7th/cmp-path" },
         {
             "hrsh7th/nvim-cmp",
-            opts = {
-                sources = {
-                    {
-                        name = "buffer",
-                        option = {
-                            get_bufnrs = function()
-                                return vim.api.nvim_list_bufs()
-                            end
-                        }
-                    },
-                    { name = "path" }
-                }
-            },
-            config = function(_, opts)
-                require("cmp").setup(opts)
-            end,
+            -- opts = {
+            --     sources = {
+            --         {
+            --             name = "buffer",
+            --             option = {
+            --                 get_bufnrs = function()
+            --                     return vim.api.nvim_list_bufs()
+            --                 end
+            --             }
+            --         },
+            --         { name = "path" }
+            --     }
+            -- },
+            -- config = function(_, opts)
+            --     require("cmp").setup(opts)
+            -- end,
         },
         { "neovim/nvim-lspconfig" },
         {
